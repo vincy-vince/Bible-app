9b21c1b6d8a9b01626463d5374f512ff741a8d13
vincy <vincyvince510@gmail.com>
2025-01-10 15:57:41 +0530

    did catalyst init and made few changes

---
+++ client/home.js
@@ -0,0 +1,3 @@
+fetch("https://www.biblegateway.com/passage/?search=Jude&version=KJV").then(
+  (res) => res.json()
+);
--- public/home.js
+++
@@ -1,2 +0,0 @@
-fetch('https://www.biblegateway.com/passage/?search=Jude&version=KJV')
-.then(res => res.json())

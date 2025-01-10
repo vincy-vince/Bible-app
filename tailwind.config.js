8bb09f4361f0bd5d8ca2500078ef3b15a7eaf570
vincy <vincyvince510@gmail.com>
2025-01-10 16:08:51 +0530

    last commit jan 10

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./client/**/*.html"],
   theme: {
     extend: {
       screens: { 'sm': { 'max': '640px' } },

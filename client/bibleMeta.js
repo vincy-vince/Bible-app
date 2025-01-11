fe02c2dd0093f6c353ba1fc97b4e848776e4b019
vineeth <vineethvince777@gmail.com>
2025-01-11 15:50:27 +0530

    last commit

---
+++ client/bibleMeta.js
@@ -0,0 +1,6 @@
+const meta = {
+  genesis:{
+    chapters:50,
+    verses: [20,32,41]
+  }
+}
--- client/home.js
+++ client/home.js
@@ -1,3 +1,10 @@
-fetch("https://www.biblegateway.com/passage/?search=Jude&version=KJV").then(
-  (res) => res.json()
-);
+// fetch("https://www.biblegateway.com/passage/?search=Jude&version=KJV").then(
+//   (res) => res.json()
+// );
+
+let chapters = document.getElementById('chapters')
+let book = document.getElementById('book')
+chapters.addEventListener('click',(event)=>{
+  // if selectbook ? "" - > genis 
+  book.style.display = 'none'
+})

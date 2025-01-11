07b92a749a0460f9fe8e3afcee450a8297a55271
vineeth <vineethvince777@gmail.com>
2025-01-11 22:25:54 +0530

    jan 11 last commit

--- client/home.js
+++ client/home.js
@@ -7,4 +7,12 @@ let book = document.getElementById('book')
 chapters.addEventListener('click',(event)=>{
   // if selectbook ? "" - > genis 
   book.style.display = 'none'
+  book.innerHTML = ''
+  let n=50;
+  for(i=1; i<=n; i++){
+    book.innerText += `
+    <div class=" mt-16 bg-amber-600 border-none rounded-full text-orange-300 w-8 h-8"><a href="genesis1.html>full</a>${i}</div>
+    `
+    book.style.display = 'block'
+  }
 })

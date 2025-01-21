320b384bb68561de9ef2f98416a5d66216f61be4
vineeth <vineethvince777@gmail.com>
2025-01-21 13:06:49 +0530

    data fetched successfully

--- client/home.js
+++ client/home.js
@@ -36,26 +36,25 @@ function updateBook(bookName) {
 
 let chapters = document.getElementById("chapters");
 let book = document.getElementById("book");
-chapters.addEventListener("click", (event) => {
-  // if selectbook ? "" - > genis
-  book.style.display = "none";
-  book.innerHTML = "";
-  let n = 50;
-  for (i = 1; i <= n; i++) {
-    book.innerHTML += `
-    <div class="div-js" id="chap-js" ><a href="dup-verse.html">${i}</a></div>
-    `;
-    book.style.display = "grid";
-    book.classList.add("book-css");
-    let chapInnerdiv = document.getElementById("chap-js");
-    chapInnerdiv.style.backgroundColor = "orange";
+//   // if selectbook ? "" - > genis
+//   book.style.display = "none";
+//   book.innerHTML = "";
+//   let n = 2;
+//   for (i = 1; i <= n; i++) {
+//     book.innerHTML += `
+//     <div class="div-js" id="chap-js" ><a href="dup-verse.html">${i}</a></div>
+//     `;
+//     book.style.display = "grid";
+//     book.classList.add("book-css");
+//     let chapInnerDiv = document.getElementById("chap-js");
+//     chapInnerDiv.style.backgroundColor = "orange";
 
-    var list = document.getElementsByClassName("div-js");
-    for (var i = 0; i < list.length; i++) {
-      list[i].classList.add("chap-css");
-    }
-  }
-});
+//     var list = document.getElementsByClassName("div-js");
+//     for (var i = 0; i < list.length; i++) {
+//       list[i].classList.add("chap-css");
+//     }
+//   }
+// });
 
 function chaptersUpdate(bookname, chap) {
   book.style.display = "none";
@@ -67,37 +66,47 @@ function chaptersUpdate(bookname, chap) {
     `;
     book.style.display = "grid";
     book.classList.add("book-css");
-    let chapInnerdiv = document.getElementById("chap-js");
-    chapInnerdiv.style.backgroundColor = "orange";
+    let chapInnerDiv = document.getElementById("chap-js");
+    chapInnerDiv.style.backgroundColor = "orange";
 
     var list = document.getElementsByClassName("div-js");
     for (var i = 0; i < list.length; i++) {
       list[i].classList.add("chap-css");
     }
   }
-  let chapInnerdiv = document.getElementById("chap-js");
-  chapInnerdiv.addEventListener("click", () => {
-    fetch(
-      `https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/${bookname}/chapters/${chap}.json`
-    )
-      .then((response) => response.json()
-      )
-      .then((data) => console.log(data)
-      
-        // console.log(data)
 
-      );
+
+  chapters.addEventListener("click", (event) => {
+    book.style.display = "none";
+    book.innerHTML = "";
+    let n = chap;
+    for (i = 1; i <= n; i++) {
+      book.innerHTML += `
+      <div class="div-js" id="chap-js" >${i}</a></div>
+      `;
+      book.style.display = "grid";
+      book.classList.add("book-css");
+      let chapInnerDiv = document.getElementById("chap-js");
+      chapInnerDiv.style.backgroundColor = "orange";
+  
+      var list = document.getElementsByClassName("div-js");
+      for (var i = 0; i < list.length; i++) {
+        list[i].classList.add("chap-css");
+      }
+    }
+
   });
+
+  let chapInnerDiv = document.querySelectorAll(".div-js");
+  chapInnerDiv.forEach(ele =>{
+    ele.addEventListener("click",()=>{
+      console.log('verse');
+      fetch( `https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/${bookname}/chapters/${chap}.json`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+    })
+  })
 }
 
-//bookArray[0].innerText
-//  english (kjv)                     ID => en-kjv
-//  Tamil - 2019[139]                 ID => ta-irvtam YES
-//  Malayalam Script                  ID => sa-Mlym-IN-laukika-sanml
-//  Hindi - 2019                      ID => hi-IN-irvhin
-//  Telugu Script                     ID => sa-Telu-IN-laukika-sante
-//  Kannada Script                    ID => sa-Knda-IN-laukika-sanka
-
-// To Do
-// according to the number of verses and chapters we should generate the HTML / the page should be updated.
+      //bookArray[0].innerText
       //https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/en-kjv/books/john/chapters/3/verses/16.json

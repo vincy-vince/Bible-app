900e6d4824da6875041067f42e395717f178004d
vineeth <vineethvince777@gmail.com>
2025-01-22 16:03:22 +0530

    almost completed avoiding duplicates

--- client/home.js
+++ client/home.js
@@ -27,9 +27,114 @@ let bookArray = document.querySelectorAll(".onBookClicked");
 
 updatedBook.addEventListener('click',() => {
 
-  book.innerHTML = "";
+  book.style.display = "none"
+  book.style.display = "block"
   book.innerHTML = `
+  <section class="mb-6 md:mb-24" id="book">
+    <section class="mt-24 mb-7 md:mt-28 lg:mt-32">
 
+      <div class="text-center text-orange-100 text-2xl lg:text-3xl font-bold mt-2">
+        Old Testament
+      </div>
+      <div class="h-svh text-orange-100 flex justify-around p-2">
+        <div>
+          <ul class="md:text-xl lg:text-xl">
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="genesis">📖 Genesis</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="exodus">📖 Exodus</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="leviticus">📖 Leviticus</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="numbers">📖 Numbers</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="deuteronomy">📖 Deuteronomy</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="joshua">📖 Joshua</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="judges">📖 Judges</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="ruth">📖 Ruth</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 samuel">📖 1 Samuel</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 samuel">📖 2 Samuel</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 kings">📖 1 Kings</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 kings">📖 2 Kings</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 chronicles">📖 1 Chronicles</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 chronicles">📖 2 Chronicles</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="ezra">📖 Ezra</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="nehemiah">📖 Nehemiah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="esther">📖 Esther</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="job">📖 Job</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="psalms">📖 Psalms</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="proverbs">📖 Proverbs</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul class="md:text-xl lg:text-xl">
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="ecclesiastes">📖 Ecclesiastes</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="song of solomon">📖 Song of Solomon</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="isaiah">📖 Isaiah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="jeremiah">📖 Jeremiah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="lamentations">📖 Lamentations</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="ezekiel">📖 Ezekiel</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="daniel">📖 Daniel</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="hosea">📖 Hosea</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="joel">📖 Joel</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="amos">📖 Amos</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="obadiah">📖 Obadiah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="jonah">📖 Jonah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="micah">📖 Micah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="nahum">📖 Nahum</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="habakkuk">📖 Habakkuk</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="zephaniah">📖 Zephaniah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="haggai">📖 Haggai</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="zechariah">📖 Zechariah</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="malachi">📖 Malachi</li>
+          </ul>
+        </div>
+      </div>
+
+    </section>
+
+    <!-- New testament -->
+    <section class="md:mt-20" id="new">
+      <div class="text-center text-orange-100 text-2xl lg:text-3xl font-bold mt-2">
+        New Testament
+      </div>
+      <div class="h-svh text-orange-100 flex justify-around p-2 pb-4">
+        <div>
+          <ul class="md:text-xl lg:text-xl new-test-books">
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="matthew">📖 Matthew</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="mark">📖 Mark</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="luke">📖 Luke</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="john">📖 John</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="acts">📖 Acts</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="romans">📖 Romans</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 corinthians">📖 1 Corinthians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 corinthians">📖 2 Corinthians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="galatians">📖 Galatians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="ephesians">📖 Ephesians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="philippians">📖 Philippians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="colossians">📖 Colossians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 thessalonians">📖 1 Thessalonians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 thessalonians">📖 2 Thessalonians</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 timothy">📖 1 Timothy</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 timothy">📖 2 Timothy</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="titus">📖 Titus</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="philemon">📖 Philemon</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="hebrews">📖 Hebrews</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="james">📖 James</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul class="md:text-xl lg:text-xl new-test-books">
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 peter">📖 1 Peter</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 peter">📖 2 Peter</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="1 john">📖 1 John</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="2 john">📖 2 John</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="3 john">📖 3 John</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="jude">📖 Jude</li>
+            <li class="onBookClicked list-none border-b-2 border-orange-100"  data-bookName="revelation">📖 Revelation</li>
+          </ul>
+        </div>
+      </div>
+
+    </section>
+  </section>
   `
 })
 
@@ -93,14 +198,27 @@ function chaptersUpdate(bookname, chap) {
         )
           .then((res) => res.json())
           .then((res) => { book.style.display = "none"
+          let totalVerse = [] 
           book.innerHTML = ""
             let array = res.data;
             array.forEach(ele => {
-              let verse = ele.verse +" "+ele.text
-              console.log(verse);
-             
+              let verseNumber = ele.verse
+            totalVerse.push(verseNumber)
+
+            let max = Math.max(...totalVerse);
+
+              let verse = verseNumber +" "+ele.text
+              if(verseNumber < max ){
+                book.innerHTML += `
+                <div class="verse-js verse-css">${verse}</div>
+                `,
+                book.style.display = "block"
+              }
+              else{
+                return
+              }
               book.innerHTML += `
-              <div class="verse-js">${verse}</div>
+              <div class="verse-js verse-css">${verse}</div>
               `
               book.style.display = "inline-block";
             })
@@ -128,14 +246,19 @@ function chaptersUpdate(bookname, chap) {
             totalVerse.push(verseNumber)
             
             let max = Math.max(...totalVerse);
+
             console.log("max " + max);
-            let verse = ele.verse +". "+ele.text
-          
+            
+          if(verseNumber < max ){
+            let verse = verseNumber +". "+ele.text
             book.innerHTML += `
             <div class="verse-js verse-css">${verse}</div>
-            `
-            book.style.display = "block";
-          
+            `,
+            book.style.display = "block"
+          }
+          else{
+            return
+          }
           })
         });
     });

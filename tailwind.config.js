5b749e1cf1fa5f801b00b7330affd962a509e16c
vincy <vincyvince510@gmail.com>
2025-01-23 14:38:59 +0530

    Revert "deleted unwanted files"
    This reverts commit ef7d46930562fc8ad1e538cab6f0a3b8cb822252.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./client/**/*.html"],
+  theme: {
+    extend: {
+      screens: { 'sm': { 'max': '640' } },
+    },
+  },
+  plugins: [],
+}
+
+
